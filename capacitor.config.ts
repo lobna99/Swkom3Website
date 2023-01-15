8f3bcb4dd3f06cd7b9c5464c00ce0b568850dc16
lobna99 <71192659+lobna99@users.noreply.github.com>
2023-01-15 14:18:51 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.parceltracker',
+  appName: 'parceltracker',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
